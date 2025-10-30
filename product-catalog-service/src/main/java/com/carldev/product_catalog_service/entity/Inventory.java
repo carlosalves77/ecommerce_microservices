7cbb41d21070876e84abed313fc39106d7cc2125
Carlos Alves <carlosaalves07@outlook.com>
2025-10-30 15:50:38 -0300

    feat: criação das entidades, Inventory, Product e Category

---
+++ product-catalog-service/src/main/java/com/carldev/product_catalog_service/entity/Inventory.java
@@ -0,0 +1,42 @@
+package com.carldev.product_catalog_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "inventory")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Inventory {
+
+    @Id
+    @Column(name = "product_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "stock_quantity", nullable = false)
+    private int stockQuantity;
+
+    @Column(name = "reserved_quantity", nullable = false)
+    private int reservedQuantity = 0;
+
+    @Column(name = "last_restocked_at")
+    private LocalDateTime lastRestockedAt;
+
+    public boolean isAvailable(int quantityNeeded) {
+        return (stockQuantity - reservedQuantity) >= quantityNeeded;
+    }
+}
