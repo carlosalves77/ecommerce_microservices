36eb06e258cbfe3e1c708560a9b2e77dbc892438
Carlos Alves <carlosaalves07@outlook.com>
2025-11-04 21:43:04 -0300

    sem alteração

--- product-catalog-service/src/main/java/com/carldev/product_catalog_service/entity/Product.java
+++ product-catalog-service/src/main/java/com/carldev/product_catalog_service/entity/Product.java
@@ -1,6 +1,7 @@
 package com.carldev.product_catalog_service.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
