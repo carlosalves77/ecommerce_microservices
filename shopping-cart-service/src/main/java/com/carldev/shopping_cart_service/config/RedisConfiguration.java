a35450eb67be32569b41198f58a0a08aa15a1830
Carlos Alves <carlosaalves07@outlook.com>
2025-11-12 15:10:39 -0300

    feat: configuração do redis

---
+++ shopping-cart-service/src/main/java/com/carldev/shopping_cart_service/config/RedisConfiguration.java
@@ -0,0 +1,27 @@
+package com.carldev.shopping_cart_service.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Value("${spring.data.redis.host}")
+    private String hostName;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        // ! You have to provide the redisStandaloneConfiguration or else the app wont
+        // ! work with docker
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(hostName);
+        redisStandaloneConfiguration.setPort(port);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+}
