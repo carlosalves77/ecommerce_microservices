24da0b57b487df66c7b38d575d26d2ae4532a4ba
carlosalves77 <carlosaalves07@outlook.com>
2025-11-28 17:17:43 -0300

    feature: auth register response e request dto

--- auth-service/src/main/java/com/carldev/auth_service/dto/request/AuthRegisterRequestDTO.java
+++ auth-service/src/main/java/com/carldev/auth_service/dto/request/AuthRegisterRequestDTO.java
@@ -1,4 +1,4 @@
-package com.carldev.auth_service.dto.authDTO.request;
+package com.carldev.auth_service.dto.request;
 
 import com.carldev.auth_service.util.RoleType;
 import jakarta.persistence.Column;
