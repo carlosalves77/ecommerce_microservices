04c27c15f1c28555108d68f215b9d9e345079fc3
carlosalves77 <carlosaalves07@outlook.com>
2025-11-28 17:20:05 -0300

    feature: auth register mapper

--- auth-service/src/main/java/com/carldev/auth_service/mapper/AuthRegisterMapper.java
+++ auth-service/src/main/java/com/carldev/auth_service/mapper/AuthRegisterMapper.java
@@ -1,7 +1,7 @@
 package com.carldev.auth_service.mapper;
 
-import com.carldev.auth_service.dto.authDTO.request.AuthRegisterRequestDTO;
-import com.carldev.auth_service.dto.authDTO.response.AuthRegisterResponseDTO;
+import com.carldev.auth_service.dto.request.AuthRegisterRequestDTO;
+import com.carldev.auth_service.dto.response.AuthRegisterResponseDTO;
 import com.carldev.auth_service.model.UserAuth;
 import org.mapstruct.Mapper;
 
