8f3648a19bc08d1ed019dee88ae9dc64fe30cec3
carlosalves77 <carlosaalves07@outlook.com>
2025-12-02 17:52:29 -0300

    feature: query para buscar usuario pelo nome e se está ativo

--- auth-service/src/main/java/com/carldev/auth_service/repository/AuthRepository.java
+++ auth-service/src/main/java/com/carldev/auth_service/repository/AuthRepository.java
@@ -2,8 +2,10 @@ package com.carldev.auth_service.repository;
 
 import com.carldev.auth_service.model.UserAuth;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -14,4 +16,9 @@ public interface AuthRepository extends JpaRepository<UserAuth, UUID> {
     boolean existsByEmail(String email);
 
     Optional<UserAuth> findByEmailIgnoreCase(String email);
+
+
+    @Query(value = "SELECT * FROM user_auth p WHERE :username % ANY(STRING_TO_ARRAY(p.username, ' ')) AND p.is_verified = :isVerified",
+            nativeQuery = true)
+    Optional<List<UserAuth>> findByUserNameAndIsVerified(String username, boolean isVerified);
 }
