ffdf54a4512c35cac954dec1221502195a17f64f
carlosalves77 <carlosaalves07@outlook.com>
2025-12-10 18:51:38 -0300

    feature: payment dto kafka consumer

---
+++ payment-service/src/main/java/com/carldev/payment_service/kafka/PaymentSuccessEvent.java
@@ -0,0 +1,35 @@
+package com.carldev.payment_service.kafka;
+
+import com.carldev.payment_service.dto.request.ItemDTO;
+import com.carldev.payment_service.dto.request.PaymentCreateEvent;
+
+import java.util.List;
+import java.util.UUID;
+
+public record PaymentSuccessEvent(
+        UUID orderId,
+        String userEmail,
+        String userName,
+        String totalAmount,
+        String currency,
+        String paymentMethod,
+        String cardLast4,
+        String paidAt,
+        List<ItemDTO> items
+) {
+
+    public static PaymentSuccessEvent fromEntity(PaymentCreateEvent requestDTO) {
+
+        return new PaymentSuccessEvent(
+                requestDTO.orderId(),
+                requestDTO.userEmail(),
+                requestDTO.userName(),
+                requestDTO.totalAmount(),
+                requestDTO.currency(),
+                requestDTO.paymentMethod(),
+                requestDTO.cardLast4(),
+                requestDTO.paidAt(),
+                requestDTO.items()
+        );
+    }
+}
