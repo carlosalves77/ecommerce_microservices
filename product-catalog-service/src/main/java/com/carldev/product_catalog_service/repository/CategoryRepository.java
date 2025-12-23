c4e6ccaf999451a286449e7942276815a7c6c7fa
carlosalves77 <carlosaalves07@outlook.com>
2025-12-22 21:05:30 -0300

    feature: adicionando query declarativa para encontrar slug

--- product-catalog-service/src/main/java/com/carldev/product_catalog_service/repository/CategoryRepository.java
+++ product-catalog-service/src/main/java/com/carldev/product_catalog_service/repository/CategoryRepository.java
@@ -3,8 +3,12 @@ package com.carldev.product_catalog_service.repository;
 import com.carldev.product_catalog_service.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     boolean existsBySlug(String slug);
+
+    List<Category> findBySlugContainingIgnoreCase(String slug);
 
 }
