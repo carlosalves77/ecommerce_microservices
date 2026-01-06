32d20d53e92a1116c0083ebbb36caeef35f0b3e9
carlosalves77 <carlosaalves07@outlook.com>
2026-01-05 23:27:48 -0300

    feature: foi criado um metodo mapping para listar categorias por paginas e slug

--- product-catalog-service/src/main/java/com/carldev/product_catalog_service/controller/CategoryController.java
+++ product-catalog-service/src/main/java/com/carldev/product_catalog_service/controller/CategoryController.java
@@ -2,13 +2,16 @@ package com.carldev.product_catalog_service.controller;
 
 import com.carldev.product_catalog_service.dto.CategoryDTO.request.CategoryRequestDTO;
 import com.carldev.product_catalog_service.dto.CategoryDTO.response.CategoryResponseDTO;
+import com.carldev.product_catalog_service.repository.CategoryRepository;
 import com.carldev.product_catalog_service.service.CategoryService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -20,10 +23,12 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping
-    public ResponseEntity<List<CategoryResponseDTO>> listAllCategories() {
+    @GetMapping("/all")
+    public ResponseEntity<Page<CategoryResponseDTO>> listAllCategories(
+            @RequestParam("page") int page
 
-        List<CategoryResponseDTO> categoryResponseDTOList = categoryService.getAllCategories();
+    ) {
+        Page<CategoryResponseDTO> categoryResponseDTOList = categoryService.getAllCategories(page);
 
         return ResponseEntity.ok().body(categoryResponseDTOList);
     }
@@ -48,6 +53,16 @@ public class CategoryController {
         CategoryResponseDTO categoryResponseDTO = categoryService.updateCategory(id, dto);
 
         return ResponseEntity.ok().body(categoryResponseDTO);
+    }
+
+    @GetMapping("/slug")
+    public ResponseEntity<List<CategoryResponseDTO>> findBySlug(
+            @RequestParam("q") String slug
+    ) {
+
+        List<CategoryResponseDTO> responseDTO = categoryService.findBySlug(slug);
+
+        return ResponseEntity.ok().body(responseDTO);
     }
 
 
