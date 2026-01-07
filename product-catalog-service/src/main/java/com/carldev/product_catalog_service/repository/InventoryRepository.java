4423f8ea779236cb524c1d08785b1cd41ef3484c
carlosalves77 <carlosaalves07@outlook.com>
2026-01-06 23:15:58 -0300

    build: dto erro para exceptions

--- product-catalog-service/src/main/java/com/carldev/product_catalog_service/repository/InventoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.carldev.product_catalog_service.repository;
-
-import com.carldev.product_catalog_service.entity.Inventory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-}
