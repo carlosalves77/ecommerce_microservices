3eae9ffbacbf38c0076f88687224fe41e1f63f74
carlosalves77 <carlosaalves07@outlook.com>
2026-01-13 12:22:24 -0300

    refactor: metodo controller para procurar produto por nome, todos os produtos, debito de produtos e rollback do produto

--- product-catalog-service/src/main/java/com/carldev/product_catalog_service/controller/ProductController.java
+++ product-catalog-service/src/main/java/com/carldev/product_catalog_service/controller/ProductController.java
@@ -1,10 +1,12 @@
 package com.carldev.product_catalog_service.controller;
 
+import com.carldev.product_catalog_service.dto.ProductDTO.request.ProductDebitRequestDTO;
 import com.carldev.product_catalog_service.dto.ProductDTO.request.ProductRequestDTO;
 import com.carldev.product_catalog_service.dto.ProductDTO.request.UpdateProductRequestDTO;
 import com.carldev.product_catalog_service.dto.ProductDTO.response.ProductResponseDTO;
 import com.carldev.product_catalog_service.service.ProductService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,19 +23,55 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping
-    public ResponseEntity<List<ProductResponseDTO>> getAllProducts() {
-        List<ProductResponseDTO> allProducts = productService.getAllProducts();
-        return ResponseEntity.ok().body(allProducts);
-    }
 
     @GetMapping("/{sku}")
     public ResponseEntity<ProductResponseDTO> getProductBySkuCriteria(
             @PathVariable String sku
     ) {
-       ProductResponseDTO productResponseSku = productService.findProductSkuByCriteria(sku);
+       ProductResponseDTO productResponseSku = productService.findProductSku(sku);
 
         return ResponseEntity.ok().body(productResponseSku);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<ProductResponseDTO>> getProductByName(
+            @RequestParam("q") String q
+    ) {
+        List<ProductResponseDTO> productList = productService.findProductByQuery(q);
+
+        return ResponseEntity.ok().body(productList);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Page<ProductResponseDTO>> getAllProducts(
+           @RequestParam("page") int page
+    ) {
+
+        Page<ProductResponseDTO> allProducts = productService.getAllProductList(page);
+
+        return ResponseEntity.status(HttpStatus.OK).body(allProducts);
+    }
+
+    @PostMapping("/payment/debit")
+    public ResponseEntity<ProductResponseDTO> getDebitInventoryQuantity(
+            @Valid
+            @RequestBody
+            ProductDebitRequestDTO productDebitRequestDTO
+    ) {
+        ProductResponseDTO productResponseDTO = productService.getProductDebit(productDebitRequestDTO);
+
+        return new ResponseEntity<>(productResponseDTO, HttpStatus.OK);
+    }
+
+    @PostMapping("/payment/rollback")
+    public ResponseEntity<ProductResponseDTO> getRollbackInventoryQuantity(
+            @RequestBody
+            ProductDebitRequestDTO productDebitRequestDTO
+    ) {
+
+        ProductResponseDTO product = productService.rollBackProduct(productDebitRequestDTO);
+
+        return ResponseEntity.ok().body(product);
     }
 
     @PostMapping
