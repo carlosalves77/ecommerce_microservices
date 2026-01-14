0f8365df0bde075cc61ad1d4b4fc48c295f87f7d
carlosalves77 <carlosaalves07@outlook.com>
2026-01-14 19:04:12 -0300

    fix: remoção do log.

--- auth-service/src/main/java/com/carldev/auth_service/config/SecurityFilter.java
+++ auth-service/src/main/java/com/carldev/auth_service/config/SecurityFilter.java
@@ -1,15 +1,18 @@
 package com.carldev.auth_service.config;
 
+import com.carldev.auth_service.dto.response.JwtUserData;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -19,21 +22,28 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     private final TokenConfig tokenConfig;
     private final UserDetailsService userDetailsService;
+    private final HandlerExceptionResolver resolver;
 
-    public SecurityFilter(TokenConfig tokenConfig, UserDetailsService userDetailsService) {
+    public SecurityFilter(TokenConfig tokenConfig, UserDetailsService userDetailsService,
+                          @Qualifier("handlerExceptionResolver")
+                          HandlerExceptionResolver resolver) {
         this.tokenConfig = tokenConfig;
         this.userDetailsService = userDetailsService;
+
+        this.resolver = resolver;
     }
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-        String path = request.getRequestURI();
-        if (path.equals("/api/auth/login") || path.equals("/api/auth/register")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
+        try {
+            String path = request.getRequestURI();
+            if (path.startsWith("/api/auth/login") || path.equals("/api/auth/register")) {
+              filterChain.doFilter(request, response);
+                return;
+            }
 
         String authorizedHeader = request.getHeader("Authorization");
         if (authorizedHeader != null && authorizedHeader.startsWith("Bearer ")) {
@@ -51,6 +61,7 @@ public class SecurityFilter extends OncePerRequestFilter {
                         new UsernamePasswordAuthenticationToken(userDetails, null,
                                 userDetails.getAuthorities());
 
+                System.out.println(authenticationToken);
 
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             } else {
@@ -58,6 +69,11 @@ public class SecurityFilter extends OncePerRequestFilter {
             }
         }
         filterChain.doFilter(request, response);
+
+        } catch (Exception e) {
+            resolver.resolveException(request, response, null, e);
+        }
+
     }
 
 }
