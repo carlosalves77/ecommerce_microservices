91ef14c86512c109237df9f08d8902c96a6b5a84
carlosalves77 <carlosaalves07@outlook.com>
2026-01-16 17:38:57 -0300

    refactor: adicionado response dto nos metodos de erros exceptions;

--- shopping-cart-service/src/main/java/com/carldev/shopping_cart_service/exception/GlobalExceptionHandler.java
+++ shopping-cart-service/src/main/java/com/carldev/shopping_cart_service/exception/GlobalExceptionHandler.java
@@ -1,67 +1,82 @@
 package com.carldev.shopping_cart_service.exception;
 
+import com.carldev.shopping_cart_service.dto.response.ErrorExceptionResponseDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<List<ErrorValidationExceptionDTO>> handleValidationException(MethodArgumentNotValidException ex) {
 
-        Map<String, String> erros = new HashMap<>();
-
-        ex.getBindingResult().getFieldErrors().forEach(erro ->
-                erros.put(erro.getField(), erro.getDefaultMessage())
-        );
-
-        return ResponseEntity.badRequest().body(erros);
+    var exceptionDTOS = ex.getBindingResult().getFieldErrors().stream().map(
+            erros -> new ErrorValidationExceptionDTO(
+                    HttpStatus.UNAUTHORIZED.value(),
+                    erros.getField(),
+                    erros.getDefaultMessage(),
+                    System.currentTimeMillis()
+            )
+    ).toList();
+        return ResponseEntity.ok().body(exceptionDTOS);
 
     }
 
     @ExceptionHandler(HandleSkuNotExistsException.class)
-    public ResponseEntity<Map<String, String>> handleIfSkuExistsException(HandleSkuNotExistsException ex) {
+    public ResponseEntity<ErrorExceptionResponseDTO> handleIfSkuExistsException(HandleSkuNotExistsException ex) {
 
-        Map<String, String> erros = new HashMap<>();
-        erros.put("Message: ", ex.getMessage());
+        ErrorExceptionResponseDTO responseDTO = new ErrorExceptionResponseDTO(
+                HttpStatus.UNAUTHORIZED.value(),
+                ex.getMessage(),
+                System.currentTimeMillis()
+        );
 
-        return ResponseEntity.badRequest().body(erros);
+        return ResponseEntity.ok().body(responseDTO);
 
     }
 
     @ExceptionHandler(HandleIfCartNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleIfCartNotFoundException(HandleIfCartNotFoundException ex) {
+    public ResponseEntity<ErrorExceptionResponseDTO> handleIfCartNotFoundException(HandleIfCartNotFoundException ex) {
 
-        Map<String, String> erro = new HashMap<>();
-        erro.put("Message: ", ex.getMessage());
+        ErrorExceptionResponseDTO responseDTO = new ErrorExceptionResponseDTO(
+                HttpStatus.UNAUTHORIZED.value(),
+                ex.getMessage(),
+                System.currentTimeMillis()
+        );
+        return ResponseEntity.ok().body(responseDTO);
 
-        return ResponseEntity.badRequest().body(erro);
     }
 
     @ExceptionHandler(HandleQuantityNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleIfQuantityNotValidException
+    public ResponseEntity<ErrorExceptionResponseDTO> handleIfQuantityNotValidException
             (HandleQuantityNotValidException ex) {
 
-        Map<String, String> erro = new HashMap<>();
+        ErrorExceptionResponseDTO responseDTO = new ErrorExceptionResponseDTO(
+                HttpStatus.UNAUTHORIZED.value(),
+                ex.getMessage(),
+                System.currentTimeMillis()
+        );
+        return ResponseEntity.ok().body(responseDTO);
 
-        erro.put("Message: ", ex.getMessage());
-
-        return ResponseEntity.badRequest().body(erro);
     }
 
     @ExceptionHandler(HandleIfCartIsEmptyException.class)
-    public ResponseEntity<Map<String, String>> handleIfCartIsEmptyException(HandleIfCartIsEmptyException ex) {
+    public ResponseEntity<ErrorExceptionResponseDTO> handleIfCartIsEmptyException(HandleIfCartIsEmptyException ex) {
 
-        Map<String, String> erro = new HashMap<>();
-
-        erro.put("Message: ", ex.getMessage());
-
-        return ResponseEntity.badRequest().body(erro);
+        ErrorExceptionResponseDTO responseDTO = new ErrorExceptionResponseDTO(
+                HttpStatus.UNAUTHORIZED.value(),
+                ex.getMessage(),
+                System.currentTimeMillis()
+        );
+        return ResponseEntity.ok().body(responseDTO);
     }
 
 }
