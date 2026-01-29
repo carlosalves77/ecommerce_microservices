2df1151293881da56bf7ea0b7f0705bca0b4135f
carlosalves77 <carlosaalves07@outlook.com>
2026-01-28 22:26:01 -0300

    refactor: remoção do dto para exception de fields e message

--- auth-service/src/main/java/com/carldev/auth_service/exception/ErrorValidationExceptionDTO.java
+++
@@ -1,9 +0,0 @@
-package com.carldev.auth_service.exception;
-
-public record ErrorValidationExceptionDTO(
-        int Status,
-        String field,
-        String message,
-        long timestamp
-) {
-}
