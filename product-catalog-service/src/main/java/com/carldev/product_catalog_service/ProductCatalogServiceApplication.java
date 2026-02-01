fd54818b5c16b2f0fdd67a76ce5d9b27a8dbd4d9
carlosalves77 <carlosaalves07@outlook.com>
2026-02-01 18:48:14 -0300

    bluid: adicionado anotação do spring security metodo

--- product-catalog-service/src/main/java/com/carldev/product_catalog_service/ProductCatalogServiceApplication.java
+++ product-catalog-service/src/main/java/com/carldev/product_catalog_service/ProductCatalogServiceApplication.java
@@ -2,8 +2,10 @@ package com.carldev.product_catalog_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 public class ProductCatalogServiceApplication {
 
 	public static void main(String[] args) {
