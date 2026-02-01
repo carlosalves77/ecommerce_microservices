761531dc4150c56acc0e2e388acae14a71ced6ba
carlosalves77 <carlosaalves07@outlook.com>
2026-02-01 18:49:28 -0300

    build: adicionado security filter chain para product service

---
+++ product-catalog-service/src/main/java/com/carldev/product_catalog_service/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.carldev.product_catalog_service.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.secret-key}")
+    private String jwtSecretKey;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.
+                authorizeHttpRequests(
+                        auth ->
+                                auth.requestMatchers(
+                                        "/v3/api-docs/**",
+                                        "/swagger-ui/**",
+                                        "/swagger-ui.html",
+                                        "/swagger-resources/**",
+                                        "/actuator/prometheus",
+                                        "/webjars/**"
+                                ).permitAll()
+
+                ).sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth ->
+                        oauth.jwt(withDefaults())
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(jwtSecretKey.getBytes(), "HmacSHA256");
+
+        return NimbusJwtDecoder.withSecretKey(secretKeySpec).build();
+    }
+
+
+}
