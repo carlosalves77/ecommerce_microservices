15aba0eae430248f3c38a71ea5d85bd5030f754c
carlosalves77 <carlosaalves07@outlook.com>
2026-02-16 14:59:40 -0300

    feat: adicionando /v1 path e nova condição em caso de erro na validação do token

--- api-gateway/src/main/java/com/carldev/api_gateway/filter/JwtValidationGatewayFilterFactory.java
+++ api-gateway/src/main/java/com/carldev/api_gateway/filter/JwtValidationGatewayFilterFactory.java
@@ -1,5 +1,7 @@
 package com.carldev.api_gateway.filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
@@ -7,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.List;
 
@@ -14,14 +17,15 @@ import java.util.List;
 public class JwtValidationGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
 
     public static final List<String> openApiEndPoints = List.of(
-            "/api/auth/login",
-            "/api/auth/register",
-            "/api/auth/verify-account",
-            "/api/auth/reset-password",
-            "/api/auth/reset-password/confirm"
+            "/api/v1/auth/login",
+            "/api/v1/auth/register",
+            "/api/v1/auth/verify-account",
+            "/api/v1/auth/reset-password",
+            "/api/v1/auth/reset-password/confirm"
     );
 
     private final WebClient webClient;
+    private static final Logger logger = LoggerFactory.getLogger(JwtValidationGatewayFilterFactory.class);
 
     public JwtValidationGatewayFilterFactory(WebClient.Builder webClientBuild,
                                              @Value("${auth.service.url}") String authServiceUrl
@@ -45,12 +49,22 @@ public class JwtValidationGatewayFilterFactory extends AbstractGatewayFilterFact
             }
 
             return webClient.get()
-                    .uri("/api/auth/validate")
+                    .uri("/api/v1/auth/validate")
                     .header(HttpHeaders.AUTHORIZATION, token)
                     .retrieve()
                     .toBodilessEntity()
                     .then(chain.filter(exchange))
                     .onErrorResume(e -> {
+                        if (e instanceof WebClientResponseException) {
+                            WebClientResponseException ex = (WebClientResponseException) e;
+                            if (ex.getStatusCode() == HttpStatus.NOT_FOUND) {
+                                logger.error("Endpoint não encontrado");
+                                exchange.getResponse().setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+                                return exchange.getResponse().setComplete();
+                            }
+
+                        }
+                        logger.error("Erro na validação do token: {}", e.getMessage());
                         exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                         return exchange.getResponse().setComplete();
                     });
