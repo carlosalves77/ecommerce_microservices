150d346d85da9b7eb436844ec41706b6ee741e56
carlosalves77 <carlosaalves07@outlook.com>
2026-02-20 23:26:04 -0300

    fix: correção do path get do feignClient

--- shopping-cart-service/src/main/java/com/carldev/shopping_cart_service/feignClient/ProductCatalogClient.java
+++ shopping-cart-service/src/main/java/com/carldev/shopping_cart_service/feignClient/ProductCatalogClient.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "product-catalog", url = "http://product-catalog-service:4004")
 public interface ProductCatalogClient {
 
-    @GetMapping("/api/product/{sku}")
+    @GetMapping("/api/v1/product/{sku}")
     ProductResponseDTO getProductBySku(@PathVariable("sku") String sku);
+
+
 
 }
