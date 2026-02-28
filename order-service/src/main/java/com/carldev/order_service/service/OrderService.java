80b0ad16f6a7d1877897f80eb1e43b92da31e41c
carlosalves77 <carlosaalves07@outlook.com>
2026-02-28 20:09:34 -0300

    feat: adicionado endereço no payment sucesso passando para kafka até o notification service

--- order-service/src/main/java/com/carldev/order_service/service/OrderService.java
+++ order-service/src/main/java/com/carldev/order_service/service/OrderService.java
@@ -3,6 +3,7 @@ package com.carldev.order_service.service;
 import com.carldev.order_service.dto.request.AddItemRequestDTO;
 import com.carldev.order_service.dto.request.CheckoutCreate;
 import com.carldev.order_service.dto.request.PaymentSuccessConsumer;
+import com.carldev.order_service.dto.response.AddressPaymentSuccess;
 import com.carldev.order_service.dto.response.DeletePaymentResponseDTO;
 import com.carldev.order_service.dto.response.OrderStatusResponseDTO;
 import com.carldev.order_service.dto.response.OrdersResponseDTO;
@@ -10,12 +11,14 @@ import com.carldev.order_service.feignClient.ProductCatalogClient;
 import com.carldev.order_service.kafka.event.OrderChangeEvent;
 import com.carldev.order_service.kafka.event.PaymentSuccessEvent;
 import com.carldev.order_service.mapper.OrderMapper;
+import com.carldev.order_service.model.Address;
 import com.carldev.order_service.model.Order;
 import com.carldev.order_service.model.OrderItem;
 import com.carldev.order_service.repository.OrderRepository;
 import com.carldev.order_service.util.OrderStatus;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.InvalidNullException;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
@@ -23,6 +26,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -66,18 +71,7 @@ public class OrderService {
             return;
         }
 
-
-        PaymentSuccessEvent paymentSuccess = new PaymentSuccessEvent(
-                orderPayment.getUserId(),
-                orderPayment.getUserName(),
-                orderPayment.getEmail(),
-                String.valueOf(orderPayment.getOrderNumber()),
-                orderPayment.getTotalAmount().toString(),
-                paymentSuccessConsumer.paymentMethod(),
-                paymentSuccessConsumer.cardLast4(),
-                paymentSuccessConsumer.paidAt(),
-                paymentSuccessConsumer.items()
-        );
+        PaymentSuccessEvent paymentSuccess = getPaymentSuccessEvent(orderPayment, paymentSuccessConsumer);
 
         orderPayment.setStatus(OrderStatus.PAID);
         orderPayment.setUpdateAt(LocalDateTime.now());
@@ -87,10 +81,38 @@ public class OrderService {
         eventPublisher.publishEvent(paymentSuccess);
     }
 
+    private static PaymentSuccessEvent getPaymentSuccessEvent(Order orderPayment,
+                                                              PaymentSuccessConsumer paymentSuccessConsumer) {
+        AddressPaymentSuccess addressPaymentSuccess = new AddressPaymentSuccess(
+             orderPayment.getOrderAddress().getStreetLine1(),
+             orderPayment.getOrderAddress().getStreetLine2(),
+             orderPayment.getOrderAddress().getCity(),
+             orderPayment.getOrderAddress().getState(),
+             orderPayment.getOrderAddress().getCountry()
+        );
+
+
+        return new PaymentSuccessEvent(
+                orderPayment.getUserId(),
+                orderPayment.getEmail(),
+                orderPayment.getUserName(),
+                String.valueOf(orderPayment.getOrderNumber()),
+                orderPayment.getTotalAmount().toString(),
+                paymentSuccessConsumer.currency(),
+                paymentSuccessConsumer.paymentMethod(),
+                paymentSuccessConsumer.cardLast4(),
+                paymentSuccessConsumer.paidAt(),
+                addressPaymentSuccess,
+                paymentSuccessConsumer.items()
+        );
+    }
+
     @KafkaListener(topics = "checkout-create", groupId = "order-service-group")
-    public void createCheckout(String messageJson) throws JsonProcessingException {
+    public void createCheckout(String messageJson
+    ) throws JsonProcessingException {
 
         CheckoutCreate checkoutCreate = objectMapper.readValue(messageJson, CheckoutCreate.class);
+
 
         Order order = new Order();
         order.setUserId(checkoutCreate.userId());
@@ -101,6 +123,16 @@ public class OrderService {
         order.setEmail(checkoutCreate.email());
         order.setOrderNumber(checkoutCreate.orderNumber());
         order.setCreatedAt(LocalDateTime.now());
+
+        Address address = new Address();
+        address.setAddressId(checkoutCreate.addressResponseDTO().addressId());
+        address.setStreetLine1(checkoutCreate.addressResponseDTO().streetLine1());
+        address.setStreetLine2(checkoutCreate.addressResponseDTO().streetLine2());
+        address.setCity(checkoutCreate.addressResponseDTO().city());
+        address.setCountry(checkoutCreate.addressResponseDTO().country());
+        address.setState(checkoutCreate.addressResponseDTO().state());
+        order.setOrderAddress(address);
+
 
         checkoutCreate.items().forEach(items ->
         {
