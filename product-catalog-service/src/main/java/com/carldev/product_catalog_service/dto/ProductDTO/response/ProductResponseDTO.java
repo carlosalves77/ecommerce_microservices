cd12d956aff4f2b08e95b9fe1b4722a369504095
carlosalves77 <carlosaalves07@outlook.com>
2026-04-09 23:00:09 -0300

    feature: adicionando serialize para conversão do objetos para leitura do redis

--- product-catalog-service/src/main/java/com/carldev/product_catalog_service/dto/ProductDTO/response/ProductResponseDTO.java
+++ product-catalog-service/src/main/java/com/carldev/product_catalog_service/dto/ProductDTO/response/ProductResponseDTO.java
@@ -1,13 +1,18 @@
 package com.carldev.product_catalog_service.dto.ProductDTO.response;
 
 import com.carldev.product_catalog_service.dto.CategoryDTO.response.CategoryResponseDTO;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Set;
 
-public record ProductResponseDTO(
+public record ProductResponseDTO  (
         String id,
         String sku,
         String name,
@@ -15,7 +20,11 @@ public record ProductResponseDTO(
         BigDecimal price,
         String imageUrl,
         boolean active,
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
         LocalDateTime createdAt,
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
         LocalDateTime updatedAt,
 
         int stockQuantity,
@@ -24,7 +33,7 @@ public record ProductResponseDTO(
         Set<CategoryResponseDTO> categories,
 
         Map<String, Object>specifications
-) {
+) implements Serializable {
 }
 
 
